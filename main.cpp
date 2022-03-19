2b0b24214696580e657d00209bb90287c6658da8
Shidqi Yusri <shidqifadhlu@gmail.com>
2022-03-19 10:18:12 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Shidqi Fadhlurrahman Yusri" << endl;
+    cout << "1301204256" << endl;
+    cout << "Telkom University" << endl;
+    
+    return 0;
+}
